055e66e4d45369f2e1e9408f185700a2f26ca8a6
Matt Wilkie <maphew@gmail.com>
2020-01-11 17:52:18 -0800

    test: add basic setup.py check

---
+++ tests/test_setup-py.cmd
@@ -0,0 +1,7 @@
+@echo   Any output other than "running check" might be an error:
+python setup.py check
+
+@goto :eof
+
+:: NOT a unit test. Todo: run from subprocess or shell util and verify 
+:: output in the standard python manner
