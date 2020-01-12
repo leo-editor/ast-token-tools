f4fc687d5c91f4af5d0f907481d1c699238917b8
Matt Wilkie <maphew@gmail.com>
2020-01-11 17:48:07 -0800

    update for "ast_token_tools" lib name

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(
 
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'': 'src'},  # Optional
+    package_dir={'': 'ast_token_tools'},  # Optional
 
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
@@ -135,7 +135,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where='src'),  # Required
+    packages=find_packages(where='ast_token_tools'),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
