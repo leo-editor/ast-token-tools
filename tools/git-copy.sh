ecb7f564c26c80fc0763817d238a855516456883
matt wilkie <maphew@gmail.com>
2020-01-18 14:41:53 -0800

    declare script purpose; credit sources
    in support of #8, describing cross-repo merge workflow

--- tools/git-copy.sh
+++ tools/git-copy.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
-# adapted from https://gist.github.com/tsayen/f1c1c4d62d4fda77abf1586bd39f9b74
+# Copy a file or directory from one git repository to another, and bring
+# the object's commit history with it.
+#
 # Usage:
+#
 #   pushd path/to/source/repo
-#   ./git-copy.sh  path/to/file/or/dir  path/to/destination/repo
+#   git-copy.sh  path/to/file/or/dir  path/to/destination/repo
+#
+# Adapted from work by @tsayen and others:
+# https://gist.github.com/tsayen/f1c1c4d62d4fda77abf1586bd39f9b74
+# https://stackoverflow.com/questions/1365541/how-to-move-files-from-one-git-repo-to-another-not-a-clone-preserving-history
+#
 echo "creating patch for path ${1}"
 git log --name-only --pretty="format:" --follow  "${1}" \
  | sort -u | \
